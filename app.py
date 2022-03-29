a7635f3b704c021582736377fbe2d5e235edf339
philpeng1 <61170302+philpeng1@users.noreply.github.com>
2022-03-29 03:35:59 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify;
 from typing import Optional;
 flask_app = Flask(__name__)
@@ -18,5 +19,5 @@ def returnname(input: Optional[str] = None):
 	return jsonify(last_name = output)
 
      
-if __name__ == "__main__":
-  app.run(debug=False)
+if __name__ == '__main__':
+    flask_app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
