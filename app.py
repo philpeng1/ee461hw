1931ca489a5f013c2658a9d6649b2790ac8e8d47
philpeng1 <61170302+philpeng1@users.noreply.github.com>
2022-03-29 03:23:38 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify;
+from flask_cors import CORS
+flask_app = Flask(__name__)
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+CORS(app)
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route("/name/<input>", methods=["GET"])
+def returnname(input: Optional[str] = None):
+	if(input == 'phil'):
+		output = 'peng'
+	else:
+		output = 'type phil'
+
+	return jsonify(last_name = output)
+
+     
+if __name__ == "__main__":
+  app.run(debug=False)
