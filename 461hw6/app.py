e511015d40438d30d4de1a09e4814fed639ef86d
philpeng1 <61170302+philpeng1@users.noreply.github.com>
2022-03-29 04:04:56 -0500

    Add files via upload

---
+++ 461hw6/app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, jsonify;
+from typing import Optional;
+flask_app = Flask(__name__)
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route("/name/")
+@app.route("/name/<input>", methods=["GET"])
+def returnname(input: Optional[str] = None):
+	if(input == 'phil'):
+		output = 'peng'
+	else:
+		output = 'type phil'
+
+	return jsonify(last_name = output)
+
+     
+if __name__ == '__main__':
+    flask_app.run(debug=True)
