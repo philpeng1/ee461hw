ddfb06261ff99c8af5e9d615ed08a17d4e37962e
philpeng1 <61170302+philpeng1@users.noreply.github.com>
2022-03-29 15:50:04 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,19 +1,15 @@
-import os
 from flask import Flask, jsonify;
 from typing import Optional;
 from flask_cors import CORS
-
 flask_app = Flask(__name__)
-app = Flask(__name__, static_folder='./build', static_url_path='/')
-
-CORS(app) #comment this on deployment
-
-@app.route('/')
+app = Flask(__name__, static_folder='./build', static_url_path='')
+CORS(app)
+@app.route('/home/')
 def index():
     return app.send_static_file('index.html')
 
-@app.route("/home/")
-@app.route("/home/<input>", methods=["GET"])
+
+@app.route("/name/<input>", methods=["GET"])
 def returnname(input: Optional[str] = None):
 	if(input == 'phil'):
 		output = 'peng'
@@ -23,5 +19,5 @@ def returnname(input: Optional[str] = None):
 	return jsonify(lastname = output)
 
      
-if __name__ == '__main__':
-    flask_app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=False)
