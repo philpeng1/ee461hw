f38727cbd882a313dd212569482c9a14ff1018e6
philpeng1 <61170302+philpeng1@users.noreply.github.com>
2022-03-29 16:47:31 -0500

    Add files via upload

--- 461hw6/src/App.js
+++ 461hw6/src/App.js
@@ -6,6 +6,7 @@ import React, {useState} from 'react'
 function App() {
    const [data, setData] = useState(null)
    const [name, setCheck] = useState(false)
+   const [error, setError] = useState("ERROR: User Not Found");
 
    function returnData(val){
      setData(val.target.value)
@@ -15,6 +16,7 @@ function App() {
    else{
      setCheck(false)
    }
+
   }
   return (
     <div className="App">
@@ -26,7 +28,7 @@ function App() {
       <input type="text" onChange={returnData} />
       </><input id="username" type="text" value={data} onChange={(e) => {
             setData(e.target.value);
-            //fetch("http://127.0.0.1:5000/home/" + e.target.value)
+              //fetch("http://127.0.0.1:5000/home/" + e.target.value)
               fetch("/home/" + e.target.value)
               .then(response => response.json())
               .then(data => { setError(data.lastname); })
